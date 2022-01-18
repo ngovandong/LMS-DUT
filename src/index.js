2101bb3a14752608872e684e377eb09e9078ba94
ngovandong <dongngo2001@gmail.com>
2022-01-18 11:08:19 +0700

    update src/index.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from "react";
 import reactDom from "react-dom";
 import App from "./App";
 import { RecoilRoot } from "recoil";
-import { HashRouter } from "react-router-dom";
+
 reactDom.render(
-  <React.StrictMode>
-    <HashRouter>
-      <RecoilRoot>
-        <App />
-      </RecoilRoot>
-    </HashRouter>
+  <React.StrictMode>zzz
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
   </React.StrictMode>,
   document.getElementById("root")
 );
